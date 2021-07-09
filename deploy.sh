09c2eb65839bfe16971396c3f27462bbf58cdf4b
Peter Sharp <psharp760@gmail.com>
2021-07-09 16:27:06 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t psharp760/multi-client:latest -t psharp760/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t psharp760/multi-server:latest -t psharp760/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t psharp760/multi-worker:latest -t psharp760/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push psharp760/multi-client:latest
+docker push psharp760/multi-server:latest
+docker psuh psharp760/multi-worker:latest
+
+docker push psharp760/multi-client:$SHA
+docker push psharp760/multi-server:$SHA
+docker psuh psharp760/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=psharp760/multi-server:$SHA
+kubectl set image deployments/client-deployment client=psharp760/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=psharp760/multi-worker:$SHA
